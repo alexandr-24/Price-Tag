63ebf1c4901287afb3ded6ab8c0e7950f7e453c5
Александр Кабыткин <akab2412@gmail.com>
2021-12-16 22:59:41 +0300

    upd

--- Price_Tag/Pages/ProductsPage.xaml.cs
+++ Price_Tag/Pages/ProductsPage.xaml.cs
@@ -13,8 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using Newtonsoft.Json;
 using Price_Tag.Classes;
+
+using IronXL;
 
 namespace Price_Tag.Pages
 {
@@ -63,8 +66,70 @@ namespace Price_Tag.Pages
                 {
                     MessageBox.Show(ex.Message.ToString());
                 }
-                
+
             }
+        }
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.Filters.Add(new CommonFileDialogFilter("Excel", "*.xls;*.xlsx"));
+            dialog.Multiselect = false;
+            
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string stream = dialog.FileName;
+                stream.Replace("/", @"\");
+                MessageBox.Show(stream);
+                ImportFromExcel(stream, true);
+            }
+        }
+        private void ImportFromExcel(string ExcelPath, bool FirstLineIsColumnName)
+        {
+            WorkBook workbook = WorkBook.Load(ExcelPath);
+            WorkSheet sheet = workbook.WorkSheets.First();
+
+
+            // Загрузка первой строки в ComboBox
+            if (FirstLineIsColumnName == true)
+            {
+                char n = 'A';
+                while (sheet[Convert.ToString(n) + Convert.ToString(1)].StringValue != "")
+                {
+                    MessageBox.Show(sheet[Convert.ToString(n) + Convert.ToString(1)].StringValue);
+                    n += (char)1;
+                }
+                n -= (char)1;
+                var firstLine = sheet["A1:" + Convert.ToString(n) + "1"].ToList();
+                CB.ItemsSource = firstLine;
+            }
+            else
+            {
+                char n = 'A';
+                List<string> lines = new List<string>();
+                while (sheet[Convert.ToString(n) + Convert.ToString(1)].StringValue != "")
+                {
+                    MessageBox.Show(sheet[Convert.ToString(n) + Convert.ToString(1)].StringValue);
+                    lines.Add(Convert.ToString(n));
+                    n += (char)1;
+                }
+                n -= (char)1;
+                var firstLine = lines;
+                CB.ItemsSource = firstLine;
+            }
+            
+            
+
+            /*
+            int k;
+            if (FirstLineIsColumnName == true) k = 2;
+            else k = 1;
+
+            while (sheet["A" + k].StringValue != "")
+            {
+                MessageBox.Show(sheet["B" + k].StringValue);
+                k++;
+            }
+            */
         }
     }
 }
