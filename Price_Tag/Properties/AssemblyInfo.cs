89ac86b99401a66175d23798c398217d9e8c9f6d
Александр Кабыткин <akab2412@gmail.com>
2021-12-25 11:19:35 +0300

    upd

--- Price_Tag/Properties/AssemblyInfo.cs
+++ Price_Tag/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Price_Tag")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Price Tag")]
+[assembly: AssemblyDescription("Программа для печати ценников")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Price_Tag")]
+[assembly: AssemblyCompany("Кабыткин Александр")]
+[assembly: AssemblyProduct("Price Tag")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -53,3 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("ru")]
